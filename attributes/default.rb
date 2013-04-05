315e3d167562fef3ce05f0500342feea9de2823a
Lars Brillert <lars@railslove.com>
2013-04-05 21:26:13 +0200

    make it be strings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
-default[:unattended_upgrades][:update_package_lists] = "1"
-default[:unattended_upgrades][:download_upgradeable_packages] = "1"
-default[:unattended_upgrades][:autoclean_interval] = "7"
-default[:unattended_upgrades][:unattended_upgrade] = "1"
+default['unattended_upgrades']['update_package_lists'] = "1"
+default['unattended_upgrades']['download_upgradeable_packages'] = "1"
+default['unattended_upgrades']['autoclean_interval'] = "7"
+default['unattended_upgrades']['unattended_upgrade'] = "1"
+default['unattended_upgrades']['allowed_origins'] = ["security"]
