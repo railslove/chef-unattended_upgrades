68f6d66915a2cf57e1ea236e2d6701ab2ee3981f
Lars Brillert <lars@railslove.com>
2013-04-05 21:19:25 +0200

    make it default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-set[:unattended_upgrades][:update_package_lists] = "1"
-set[:unattended_upgrades][:download_upgradeable_packages] = "1"
-set[:unattended_upgrades][:autoclean_interval] = "7"
-set[:unattended_upgrades][:unattended_upgrade] = "1"
+default[:unattended_upgrades][:update_package_lists] = "1"
+default[:unattended_upgrades][:download_upgradeable_packages] = "1"
+default[:unattended_upgrades][:autoclean_interval] = "7"
+default[:unattended_upgrades][:unattended_upgrade] = "1"
