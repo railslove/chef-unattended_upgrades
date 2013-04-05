e69c8f045413afb426174451be897e14cf6d8902
Lars Brillert <lars@railslove.com>
2013-04-05 21:45:24 +0200

    customizable blacklist

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['unattended_upgrades']['download_upgradeable_packages'] = 1
 default['unattended_upgrades']['autoclean_interval'] = 7
 default['unattended_upgrades']['unattended_upgrade'] = 1
 default['unattended_upgrades']['allowed_origins'] = ["security"]
+default['unattended_upgrades']['package_blacklist'] = []
